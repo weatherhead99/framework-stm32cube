f040fa80755079a1d869d9d99d3309ed95edf2c4
Dan <dan.weatherill@cantab.net>
2020-08-03 01:01:30 +0100

    add g4, mp1 and wb downloads to update script

--- update_all.sh
+++ update_all.sh
@@ -6,7 +6,11 @@
 ./update.sh git@github.com:STMicroelectronics/STM32CubeF4.git f4
 ./update.sh git@github.com:STMicroelectronics/STM32CubeF7.git f7
 ./update.sh git@github.com:STMicroelectronics/STM32CubeG0.git g0
+./update.sh git@github.com:STMicroelectronics/STM32CubeG4.git g4
 ./update.sh git@github.com:STMicroelectronics/STM32CubeH7.git h7
 ./update.sh git@github.com:STMicroelectronics/STM32CubeL0.git l0
 ./update.sh git@github.com:STMicroelectronics/STM32CubeL1.git l1
 ./update.sh git@github.com:STMicroelectronics/STM32CubeL4.git l4
+./update.sh git@github.com:STMicroelectronics/STM32CubeL5.git l5
+./update.sh git@github.com:STMicroelectronics/STM32CubeMP1.git mp1
+./update.sh git@github.com:STMicroelectronics/STM32CubeWB.git wb
